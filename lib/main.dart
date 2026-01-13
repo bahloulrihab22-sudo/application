2d5734efcde73e13f7afef8668c76d4181f9f7e1
bahloulrihab22-sudo <bahloulrihab22@gmail.com>
2026-01-13 17:33:01 +0100

    INITIAL COMMIT

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Couple App',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Couple App ❤️'),
+      ),
+      body: const Center(
+        child: Text(
+          'Bienvenue dans notre application 💕',
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
